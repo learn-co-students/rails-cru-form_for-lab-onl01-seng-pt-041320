495805c619d21d7ec4f3b1d03d0a233741e64e64
marcusLau <marcus.lau76@gmail.com>
2020-08-24 16:50:07 -0700

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,31 @@
 class ArtistsController < ApplicationController
+
+    def show 
+        @artist = Artist.find(params[:id])
+    end
+
+    def new 
+        @artist = Artist.new
+    end 
+
+    def create
+        @artist = Artist.create(artist_params) # this also saves/inserts into db 
+        @artist.save
+        redirect_to artist_path(@artist)
+    end
+
+    def edit
+        @artist = Artist.find(params[:id])
+    end
+
+    def update
+        @artist = Artist.find(params[:id])
+        @artist.update(artist_params)
+        redirect_to artist_path(@artist)
+    end
+
+    def artist_params
+        params.require(:artist).permit(:name, :bio)
+    end
+
 end
