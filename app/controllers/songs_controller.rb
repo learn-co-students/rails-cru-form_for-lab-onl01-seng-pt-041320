be1661cf4d2025c468591f07b3854545056f2590
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-08-30 14:05:25 -0700

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,21 @@
+class SongsController < ApplicationController
+    def index
+        @songs = Song.all
+    end
+
+    def new
+    end
+    
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def show
+        @song = Song.find(params[:id])
+    end
+end
