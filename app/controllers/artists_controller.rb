37d14f4e9e51318cf7526e1e0f77369a9144016a
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-08-30 13:43:08 -0700

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -11,4 +11,10 @@ class ArtistsController < ApplicationController
     def show
         @artist = Artist.find(params[:id])
     end
+
+    def create
+        @artist = Artist.new(params.require(:artist).permit(:name, :bio))
+        @artist.save
+        redirect_to artist_path(@artist)
+    end
 end
