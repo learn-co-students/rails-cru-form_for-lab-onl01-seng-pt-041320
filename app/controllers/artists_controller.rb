c18d544d899eb3a436d01a5e065a2ecd71efcad5
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-08-30 12:56:37 -0700

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,14 @@
+class ArtistsController < ApplicationController
+
+    def index
+        @artists = Artist.all
+    end
+
+    def new
+        @artist = Artist.new
+    end
+
+    def show
+        @artist = Artist.find(params[:id])
+    end
+end
