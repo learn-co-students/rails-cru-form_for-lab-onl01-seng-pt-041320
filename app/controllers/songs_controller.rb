4bfd3f1d47a0ca388ede1543c43337004055d97a
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-08-30 16:20:04 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,9 +4,13 @@ class SongsController < ApplicationController
     end
 
     def new
+        @song = Song.new
     end
     
     def create
+        @song = Song.new(params.require(:song).permit(:name, :artist_id, :genre_id))
+        @song.save
+        redirect_to song_path(@song)
     end
 
     def edit
